f8933c73fe2c71748c8c5573cd2444fbc2e472c3
Harold Martinez <haroldmt@outlook.com>
2024-12-10 22:55:59 -0500

    Primer cargue del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './layout/login/login.component';
+import { AdminusuarioComponent } from './layout/adminusuario/adminusuario.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/layout', pathMatch:'full'},
+    { path: 'layout', component: LayoutComponent, children: [
+        { path: 'login', component: LoginComponent },
+        { path: 'adminusuario', component: AdminusuarioComponent }
+      ]}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/layout/adminusuario/adminusuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminusuario',
+  imports: [],
+  templateUrl: './adminusuario.component.html',
+  styleUrl: './adminusuario.component.css'
+})
+export class AdminusuarioComponent {
+
+}
